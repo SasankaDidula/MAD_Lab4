049423409a9dbee2757be17c268a0f4a0b88183f
SasankaDidula <61312369+SasankaDidula@users.noreply.github.com>
2020-03-20 23:50:21 +0530

    Labsheet 3 Final

--- app/src/main/java/com/e/labsheet04/Registration.java
+++ app/src/main/java/com/e/labsheet04/Registration.java
@@ -2,7 +2,10 @@ package com.e.labsheet04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registration extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Registration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        final Intent intent = new Intent(Registration.this, MainActivity.class);
+        Button cancel_btn = (Button) findViewById(R.id.cancel_btn);
+        cancel_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
